a8a4658ea8153d7181cac4be6f9051e2dbfc1ee4
Prashant Bhushan <43017232+pra-bhu@users.noreply.github.com>
2019-07-07 20:49:31 +0530

    Change in style for animation
    Add comment in components

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,47 +1,54 @@
 import React, { Component } from "react";
 import Section from "./Section";
 import { connect } from "react-redux";
-
+/* 
+Component to create Collapsible Accordion.
+ */
 class Accordion extends Component {
-	constructor(props) {
-		super(props);
-		this.handleActive = this.handleActive.bind(this);
-		this.state = {
-			sections: props.sections
-		};
-	}
-
-	handleActive(caption) {
-		this.setState(prevState => ({
-			sections: prevState.sections.map(prev => ({
-				icon: prev.icon,
-				caption: prev.caption,
-				isActive:
-					prev.caption === caption ? (prev.isActive ? false : true) : false,
-				detail: prev.detail
-			}))
-		}));
-	}
-	render() {
-		let key = 0;
-		let sections = this.state.sections.map(curr => (
-			<Section
-				key={key++}
-				prop={{
-					icon: curr.icon,
-					isActive: curr.isActive,
-					caption: curr.caption,
-					handleActive: this.handleActive,
-					detail: curr.detail
-				}}
-			/>
-		));
-		return <div className="accordion">{sections}</div>;
-	}
+  constructor(props) {
+    super(props);
+    this.handleActive = this.handleActive.bind(this);
+    this.state = {
+      sections: props.sections
+    };
+  }
+  /* 
+	Recieves Click event with the caption of the section
+	Sets the section with the flag value and removes flag from
+	previous sections.
+ */
+  handleActive(caption) {
+    this.setState(prevState => ({
+      sections: prevState.sections.map(prev => ({
+        icon: prev.icon,
+        caption: prev.caption,
+        isActive: prev.caption === caption ? !prev.isActive : false,
+        detail: prev.detail
+      }))
+    }));
+  }
+  render() {
+    let key = 0;
+    let sections = this.state.sections.map(curr => (
+      <Section
+        key={key++}
+        prop={{
+          icon: curr.icon,
+          isActive: curr.isActive,
+          caption: curr.caption,
+          handleActive: this.handleActive,
+          detail: curr.detail
+        }}
+      />
+    ));
+    return <div className="accordion">{sections}</div>;
+  }
 }
 
+//Pulls the state from Redux-store and populates with the
+//required list of sections.
 const mapStateToProps = (state, ownProps) => ({
-	sections: state.sections
+  sections: state.sections
 });
 
 export default connect(mapStateToProps)(Accordion);
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,36 +2,38 @@ import React, { Component } from "react";
 import propTypes from "prop-types";
 import "../style/style.css";
 
+//Component to render the section
 class Section extends Component {
-	render() {
-		const { isActive, caption, handleActive, icon, detail } = this.props.prop;
-		return (
-			<div>
-				<div
-					className={isActive ? "section active" : "section"}
-					onClick={() => handleActive(caption)}
-				>
-					<div className="flexBox">
-						<i className="material-icons">{icon}</i>
-						<div className="caption">{caption}</div>
-					</div>
-				</div>
-				<div className={isActive ? "section_detail expand" : "section_detail"}>
-					{detail}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const { isActive, caption, handleActive, icon, detail } = this.props.prop;
+    return (
+      <div>
+        <div
+          className={isActive ? "section active" : "section"}
+          onClick={() => handleActive(caption)}
+        >
+          <div className="flexBox">
+            <i className="material-icons">{icon}</i>
+            <div className="caption">{caption}</div>
+          </div>
+        </div>
+        {/* Checks if the section is to be expanded*/}
+        <div className={isActive ? "section_detail expand" : "section_detail"}>
+          {detail}
+        </div>
+      </div>
+    );
+  }
 }
-
+//Validating prop-types of the object passed as prop
 Section.propTypes = {
-	prop: propTypes.shape({
-		isActive: propTypes.bool.isRequired,
-		caption: propTypes.string.isRequired,
-		handleActive: propTypes.func.isRequired,
-		icon: propTypes.string.isRequired,
-		detail: propTypes.string.isRequired
-	}).isRequired
+  prop: propTypes.shape({
+    isActive: propTypes.bool.isRequired,
+    caption: propTypes.string.isRequired,
+    handleActive: propTypes.func.isRequired,
+    icon: propTypes.string.isRequired,
+    detail: propTypes.string.isRequired
+  }).isRequired
 };
 
 export default Section;
